85269b93b1bb12da5706e8ed6ba9be9ed66fb218
Denis Komarkov <denis.komarkov@gmail.com>
2025-05-14 01:45:25 +0300

    feat(nvim/blink.cmp): enable documentation auto show

--- .config/nvim/lua/config/plugins/blink.cmp.lua
+++ .config/nvim/lua/config/plugins/blink.cmp.lua
@@ -10,7 +10,12 @@ return {
             appearance = {
                 nerd_font_variant = 'mono'
             },
-            fuzzy = { implementation = "lua" }
+            fuzzy = { implementation = "lua" },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                }
+            }
         },
     }
 }
