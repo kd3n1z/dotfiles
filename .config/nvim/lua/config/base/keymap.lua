e4cdd9a81b1d30003628ea5a73bd047b39acf4c1
Denis Komarkov <denis.komarkov@gmail.com>
2025-07-13 17:23:44 +0300

    fix(nvim/keymap): change D-. to A-.

--- .config/nvim/lua/config/base/keymap.lua
+++ .config/nvim/lua/config/base/keymap.lua
@@ -1,11 +1,10 @@
-local telescope_builtin = require('telescope.builtin')
+local telescope = require('telescope.builtin')
 
 -- lsp
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
-vim.keymap.set("n", "<F12>", telescope_builtin.lsp_references)
-vim.keymap.set("n", "<D-.>", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<F12>", telescope.lsp_references)
+vim.keymap.set("n", "<A-.>", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<MS-f>", vim.lsp.buf.format)
 
 -- files
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<F1>', function () telescope.find_files({ hidden = true }) end)
+vim.keymap.set('n', '<F1>', function() telescope.find_files({ hidden = true }) end)
